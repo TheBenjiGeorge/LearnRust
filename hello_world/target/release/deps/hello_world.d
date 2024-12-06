42251207c383d4c4db17108e07b6fc43d9542c48
TheBenjiGeorge <150831016+TheBenjiGeorge@users.noreply.github.com>
2024-12-06 23:20:41 +1100

    Made Hello World

---
+++ hello_world/target/release/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\thebe\Documents\GitHub\LearnRust\hello_world\target\release\deps\hello_world.exe: src/main.rs
+
+C:\Users\thebe\Documents\GitHub\LearnRust\hello_world\target\release\deps\hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\thebe\Documents\GitHub\LearnRust\hello_world\target\release\hello_world.exe: C:\Users\thebe\Documents\GitHub\LearnRust\hello_world\src\main.rs
